f5a508d49e4abdcd8105fe1531e63787b40dd106
baitutanglj <2919588049@qq.com>
2023-04-19 11:26:42 +0800

    commit data

---
+++ dd-project/iteration_1/simple_job/simple_job_11.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/linjie/projects/D2/pd_python
+/public/software/.local/easybuild/software/Anaconda3/2020.02/envs/rdkit/bin/python 8.2progressive_docking.py -num_units 2000 -dropout 0.7 -learn_rate 0.0001 -bin_array 3 -wt 2 -cf -11.2 -n_it 1 -t_mol 401.3399 -os 10 -bs 256 -protein dd-project -file_path /home/linjie/projects/D2 -run_time 1 -vl 10000
---
+++ dd-project/iteration_1/simple_job_predictions/simple_job_1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/linjie/projects/D2/pd_python
+/public/software/.local/easybuild/software/Anaconda3/2020.02/envs/rdkit/bin/python 11.2Prediction_morgan_1024.py -mname BE_smiles.txt -id BE_smiles.txt -protein dd-project -n_it 1 -file_path /home/linjie/projects/D2 -dd /home/linjie/projects/D2/zinc1/morgan/morgan -chs 1000000
